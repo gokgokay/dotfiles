70506101c7d1a46a133214bf825c16005d87dd2e
Gokay Gok <gokgokay35@gmail.com>
2024-07-17 04:22:34 +0300

    Add bufremove to bufferline

--- nvim/lua/plugins/buffers.lua
+++ nvim/lua/plugins/buffers.lua
@@ -1,22 +1,8 @@
 return {
-  {
+	{
 		"akinsho/bufferline.nvim",
-    dependencies = "echasnovski/mini.bufremove",
+		dependencies = "echasnovski/mini.bufremove",
 		event = "VeryLazy",
-		keys = {
-			{ "<s-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
-			{ "<s-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-			{ "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
-			{ "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-			{ "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
-			{ "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
-			{ "<leader>bb", "<cmd>e #<cr>", desc = "Switch buffer"},
-			{ "<leader>bo", "<cmd>BufferLineCloseOthers<cr>", desc = "Delete other buffers" },
-			{ "<leader>bP", "<cmd>BufferLineGroupClose ungrouped<cr>", desc = "Delete non-pinned buffers" },
-			{ "<leader>bp", "<cmd>BufferLineTogglePin<cr>", desc = "Toggle pin buffer" },
-      { "<leader>br", "<cmd>BufferLineCloseRight<cr>", desc = "Delete right buffer"},
-      { "<leader>bl", "<cmd>BufferLineCloseLeft<cr>", desc = "Delete left buffer"},
-		},
 		opts = {
 			options = {
 				indicator = {
@@ -43,7 +29,7 @@ return {
 			},
 		},
 		config = function(_, opts)
-      local bufferline = require("bufferline")
+			local bufferline = require("bufferline")
 			local macchiato = require("catppuccin.palettes").get_palette("macchiato")
 			opts.highlights = require("catppuccin.groups.integrations.bufferline").get({
 				styles = { "italic", "bold" },
@@ -60,5 +46,19 @@ return {
 			})
 			bufferline.setup(opts)
 		end,
+		keys = {
+			{ "<s-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+			{ "<s-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+			{ "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+			{ "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+			{ "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+			{ "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+			{ "<leader>bb", "<cmd>e #<cr>", desc = "Switch buffer" },
+			{ "<leader>bo", "<cmd>BufferLineCloseOthers<cr>", desc = "Delete other buffers" },
+			{ "<leader>bP", "<cmd>BufferLineGroupClose ungrouped<cr>", desc = "Delete non-pinned buffers" },
+			{ "<leader>bp", "<cmd>BufferLineTogglePin<cr>", desc = "Toggle pin buffer" },
+			{ "<leader>br", "<cmd>BufferLineCloseRight<cr>", desc = "Delete right buffer" },
+			{ "<leader>bl", "<cmd>BufferLineCloseLeft<cr>", desc = "Delete left buffer" },
+		},
 	},
 }
