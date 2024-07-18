d00ceac661db2bf8c2473c19f051a9d671fc33a1
Gokay Gok <gokgokay35@gmail.com>
2024-07-18 18:09:38 +0300

    Add catppuccin theme to bufferline

--- nvim/lua/plugins/buffers.lua
+++ nvim/lua/plugins/buffers.lua
@@ -1,49 +1,43 @@
 return {
 	{
 		"akinsho/bufferline.nvim",
-		dependencies = "echasnovski/mini.bufremove",
 		event = "VeryLazy",
-		opts = {
-			options = {
-				indicator = {
-					icon = "▎",
-					style = "icon",
-				},
-				mode = "buffers",
-				show_buffer_icons = false,
-				always_show_bufferline = false,
-				diagnostics = "nvim_lsp",
-				max_name_length = 18,
-				max_prefix_length = 15,
-				tab_size = 18,
-				separator_style = "thin",
-				offsets = {
-					{
-						filetype = "NvimTree",
-						text = "File Explorer",
-						highlight = "Directory",
-						text_align = "center",
-						separator = true,
+		dependencies = "echasnovski/mini.bufremove",
+		opts = function()
+			local macchiato = require("catppuccin.palettes").get_palette("macchiato")
+			return {
+				highlights = require("catppuccin.groups.integrations.bufferline").get({
+					styles = { "italic", "bold" },
+					custom = {
+						macchiato = {
+							background = { fg = macchiato.text },
+						},
+					},
+				}),
+				options = {
+					mode = "buffers",
+					enforce_regular_tabs = true,
+					show_buffer_icons = true,
+					show_close_icon = false,
+					show_buffer_close_icons = false,
+					always_show_bufferline = false,
+					diagnostics = "nvim_lsp",
+					max_name_length = 18,
+					max_prefix_length = 15,
+					tab_size = 18,
+					separator_style = "thin",
+					offsets = {
+						{
+							filetype = "NvimTree",
+							text = "File Explorer",
+							text_align = "center",
+						},
 					},
 				},
-			},
-		},
+			}
+		end,
 		config = function(_, opts)
 			local bufferline = require("bufferline")
-			local macchiato = require("catppuccin.palettes").get_palette("macchiato")
-			opts.highlights = require("catppuccin.groups.integrations.bufferline").get({
-				styles = { "italic", "bold" },
-				custom = {
-					all = {
-						fill = {
-							-- bg = "",
-						},
-						background = {
-							fg = macchiato.text,
-						},
-					},
-				},
-			})
 			bufferline.setup(opts)
 		end,
 		keys = {
@@ -54,6 +48,8 @@ return {
 			{ "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
 			{ "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
 			{ "<leader>bb", "<cmd>e #<cr>", desc = "Switch buffer" },
+      { "<leader>bd", function() require("mini.bufremove").delete(0, false) end, desc = "Delete buffer" },
+      { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete buffer (Force)" },
 			{ "<leader>bo", "<cmd>BufferLineCloseOthers<cr>", desc = "Delete other buffers" },
 			{ "<leader>bP", "<cmd>BufferLineGroupClose ungrouped<cr>", desc = "Delete non-pinned buffers" },
 			{ "<leader>bp", "<cmd>BufferLineTogglePin<cr>", desc = "Toggle pin buffer" },
