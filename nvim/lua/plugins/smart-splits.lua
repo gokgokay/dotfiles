c53fb1254e3c89e6f65c6ec1ba67a574fe01822f
Gokay Gok <gokgokay35@gmail.com>
2024-07-19 04:00:45 +0300

    Add smart-splits plugin

---
+++ nvim/lua/plugins/smart-splits.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"mrjones2014/smart-splits.nvim",
+		event = "VeryLazy",
+		keys = {
+			{ "<A-l>", "<cmd>SmartResizeRight<cr>", desc = "Resize right" },
+			{ "<A-k>", "<cmd>SmartResizeUp<cr>", desc = "Resize up" },
+			{ "<A-j>", "<cmd>SmartResizeDown<cr>", desc = "Resize down" },
+			{ "<A-h>", "<cmd>SmartResizeLeft<cr>", desc = "Resize down" },
+		},
+	},
+}
