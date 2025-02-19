db350ba0ebc036eb5e7f217596ecb29a4e9276f8
Gokay Gok <gokgokay35@gmail.com>
2025-02-19 17:40:43 +0300

    refactor(coding): remove unnecessary nvim-autopairs config

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,5 +1,17 @@
 return {
-  -- Completion of lsp and snippets
+	-- Complete parentheses
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		dependencies = "hrsh7th/nvim-cmp",
+		config = function()
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
+
+	-- Completion of lsp and snippets
 	{
 		"hrsh7th/nvim-cmp",
 		event = "InsertEnter",
@@ -121,26 +133,11 @@ return {
 		end,
 	},
 
-  -- Complete parentheses
+	-- Surround
 	{
-		"windwp/nvim-autopairs",
-		event = { "InsertEnter" },
-		dependencies = "hrsh7th/nvim-cmp",
-
-		config = function()
-			local npairs = require("nvim-autopairs")
-			npairs.setup({
-				check_ts = true,
-				enable_check_bracket_line = false,
-				ts_config = {
-					lua = { "string" },
-					javascript = { "template_string" },
-					java = false,
-				},
-			})
-			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-			local cmp = require("cmp")
-			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-		end,
+		"kylechui/nvim-surround",
+		version = "*",
+		event = { "BufReadPre", "BufNewFile" },
+		config = true,
 	},
 }
