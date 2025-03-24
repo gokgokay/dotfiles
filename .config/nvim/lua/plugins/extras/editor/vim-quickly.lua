a7936d8960ba5e6bb1b1d8004f02d4f67873db0d
Gokay Gok <gokgokay35@gmail.com>
2025-03-24 14:11:06 +0300

    feat(vim-quickhl): update mappings

--- .config/nvim/lua/plugins/extras/editor/vim-quickly.lua
+++ .config/nvim/lua/plugins/extras/editor/vim-quickly.lua
@@ -5,10 +5,8 @@ return {
 		keys = {
 			{ "<S-n>", "<Plug>(quickhl-manual-go-to-next)", mode = { "n", "x" }, desc = "Next highlighted word" },
 			{ "<S-p>", "<Plug>(quickhl-manual-go-to-prev)", mode = { "n", "x" }, desc = "Prev highlighted word" },
-			{ "]h", "<Plug>(quickhl-manual-go-to-next)", mode = { "n", "x" }, desc = "Next highlighted word" },
-			{ "[h", "<Plug>(quickhl-manual-go-to-prev)", mode = { "n", "x" }, desc = "Prev highlighted word" },
-			{ "ghh", "<Plug>(quickhl-manual-this)", mode = { "n", "x" }, desc = "Highlight word" },
-			{ "ghr", "<Plug>(quickhl-manual-reset)", mode = { "n", "x" }, desc = "Reset highlighted words" },
+			{ "gh", "<Plug>(quickhl-manual-this)", mode = { "n", "x" }, desc = "Highlight words" },
+			{ "gH", "<Plug>(quickhl-manual-reset)", mode = { "n", "x" }, desc = "Reset highlighted words" },
 		},
 	},
 }
