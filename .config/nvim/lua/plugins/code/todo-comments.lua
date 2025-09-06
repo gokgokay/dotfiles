a48548e5383bb7114a1248e87db2b543b22b4b13
Gokay Gok <gokgokay35@gmail.com>
2025-09-06 15:08:45 +0300

    fix(todo-comments): ensure plugin loads correctly

--- .config/nvim/lua/plugins/code/todo-comments.lua
+++ .config/nvim/lua/plugins/code/todo-comments.lua
@@ -1,12 +1,13 @@
 return {
 	-- Highlight todo, notes, etc in comments
-	{
-		"folke/todo-comments.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = { "nvim-lua/plenary.nvim" },
-		keys = {
-			{ "<leader>st", "<cmd>TodoTelescope keywords=TODO,FIX<cr>", desc = "Todo/Fix" },
-			{ "<leader>sT", "<cmd>TodoTelescope<cr>", desc = "All Todos" },
-		},
+	"folke/todo-comments.nvim",
+	event = "VeryLazy",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = {
+		{ "<leader>st", "<cmd>TodoTelescope keywords=TODO,FIX<cr>", desc = "Todo/Fix" },
+		{ "<leader>sT", "<cmd>TodoTelescope<cr>", desc = "All Todos" },
 	},
+	config = function()
+		require("todo-comments").setup()
+	end,
 }
