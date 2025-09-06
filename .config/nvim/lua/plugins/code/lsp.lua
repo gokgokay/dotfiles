5a807f7b0b6ee4379ae79ce4b2f033f9d9695b49
Gokay Gok <gokgokay35@gmail.com>
2025-09-06 15:01:04 +0300

    fix(lsp): fix undefined_global "vim" error

--- .config/nvim/lua/plugins/code/lsp.lua
+++ .config/nvim/lua/plugins/code/lsp.lua
@@ -3,37 +3,18 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		lazy = false,
+		dependencies = { "williamboman/mason-lspconfig.nvim" },
 		config = function()
 			require("config.lsp").setup()
 		end,
 	},
-
-	-- Manage lsp, linters, and formatters
 	{
 		"williamboman/mason.nvim",
-		lazy = false,
 		opts = {
 			max_concurrent_installers = 10,
 		},
-		keys = {
-			{ "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" },
-		},
+		keys = { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" },
 	},
-
-	-- Auto-configuring installed LSP servers
-	{
-		"mason-org/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {},
-	},
-
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    config = function()
-      require("mason-lspconfig").setup {}
-    end,
-  },
 
 	-- Auto-install lsp, linters and formatters
 	{
