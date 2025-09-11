c1a633950d684293511619745e9b35cc7d767e32
Gokay Gok <gokgokay35@gmail.com>
2025-09-11 19:16:06 +0300

    refactor(lualine): update layout, separators, and sections

--- .config/nvim/lua/plugins/ui/lualine.lua
+++ .config/nvim/lua/plugins/ui/lualine.lua
@@ -8,27 +8,26 @@ return {
 			"nvim-tree/nvim-web-devicons",
 			"arkav/lualine-lsp-progress",
 		},
-		opts = function()
-			return {
-				options = {
-					theme = "auto",
-					section_separators = { left = "", right = "" },
-					disabled_filetypes = { statusline = { "dashboard", "alpha", "lazy" } },
-					globalstatus = true,
-				},
-				sections = {
-					lualine_a = { "mode" },
-					lualine_b = { "branch", "diff", "diagnostics" },
-					lualine_c = { "filename" },
-					lualine_x = { "fileformat" },
-					lualine_y = { "progress", "location" },
-					lualine_z = { { "datetime", style = "%H:%M" } },
-				},
-				extensions = { "neo-tree", "nvim-tree", "toggleterm", "quickfix" },
-			}
-		end,
-		config = function(_, opts)
-			require("lualine").setup(opts)
-		end,
+		opts = {
+			options = {
+				theme = "auto",
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
+				disabled_filetypes = { statusline = { "dashboard", "lazy" } },
+				globalstatus = true,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff" },
+				lualine_c = { { "filename", path = 1 } },
+				lualine_x = { "diagnostics", "filetype" },
+				lualine_y = { "encoding", "fileformat" },
+				lualine_z = { "location", "progress" },
+			},
+			inactive_sections = {
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+			},
+		},
 	},
 }
