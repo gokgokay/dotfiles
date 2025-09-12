d619d4f7310def98145155853a856d44723012d0
Gokay Gok <gokgokay35@gmail.com>
2025-09-12 09:48:13 +0300

    fix(bufferline): new catppuccin integration

--- .config/nvim/lua/plugins/ui/bufferline.lua
+++ .config/nvim/lua/plugins/ui/bufferline.lua
@@ -4,46 +4,25 @@ return {
 		"akinsho/bufferline.nvim",
 		dependencies = { "echasnovski/mini.bufremove" },
 		event = "VeryLazy",
-		opts = function()
-			local palette = require("catppuccin.palettes").get_palette("macchiato")
-			local bufferline_hl = require("catppuccin.groups.integrations.bufferline").get({
-				styles = { "bold" },
-				custom = {
-					all = {
-						fill = { bg = palette.mantle },
-					},
-				},
-			})
-			return {
-				highlights = bufferline_hl,
-				options = {
-					diagnostics = "nvim_lsp",
-					show_buffer_icons = true,
-					enforce_regular_tabs = true,
-					always_show_bufferline = false,
-					show_close_icon = false,
-					show_buffer_close_icons = false,
-					separator_style = "thin",
-					offsets = {
-						{
-							filetype = "NvimTree",
-							text = "File Explorer",
-							text_align = "center",
-							highlight = "Directory",
-							padding = 1,
-						},
-					},
-				},
-			}
+		init = function()
+			local bufline = require("catppuccin.groups.integrations.bufferline")
+			function bufline.get()
+				return bufline.get_theme()
+			end
 		end,
+		opts = {
+			options = {
+				diagnostics = "nvim_lsp",
+				always_show_bufferline = false,
+				diagnostics_indicator = function(count, level)
+					local icon = level:match("error") and " " or " "
+					return " " .. icon .. count
+				end,
+			},
+		},
 		keys = {
-			-- Buffer navigation
 			{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
 			{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-			-- Buffer reordering
-			{ "<leader>bmj", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer right" },
-			{ "<leader>bmk", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer left" },
-			-- Buffer management
 			{ "<leader>bb", "<cmd>e #<cr>", desc = "Switch buffer" },
 			{
 				"<leader>bd",
@@ -53,18 +32,15 @@ return {
 				desc = "Delete buffer",
 			},
 			{
-				"<leader>bC",
+				"<leader>bD",
 				function()
 					require("mini.bufremove").delete(0, true)
 				end,
-				desc = "Force close buffer",
+				desc = "Force delete buffer",
 			},
 			{ "<leader>bo", "<cmd>BufferLineCloseOthers<cr>", desc = "Close other buffers" },
 			{ "<leader>bp", "<cmd>BufferLineTogglePin<cr>", desc = "Toggle pin buffer" },
 			{ "<leader>bP", "<cmd>BufferLineGroupClose ungrouped<cr>", desc = "Close non-pinned buffers" },
 		},
-		config = function(_, opts)
-			require("bufferline").setup(opts)
-		end,
 	},
 }
--- .config/nvim/lua/plugins/ui/dashboard.lua
+++ .config/nvim/lua/plugins/ui/dashboard.lua
@@ -5,7 +5,6 @@ return {
 		lazy = false,
 		event = "VimEnter",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-
 		opts = function()
 			local logo = [[
 ██╗   ██╗██╗███╗   ███╗
