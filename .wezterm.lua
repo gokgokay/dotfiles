3b3aa9eeccedc4f9b812452f9a7f69b40dee470b
Gokay Gok <gokgokay35@gmail.com>
2025-09-12 13:32:23 +0300

    chore(wezterm): some minor changes

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,23 +2,30 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
-config.window_background_opacity = 1
-config.automatically_reload_config = true
-
--- set theme
-config.color_scheme = "Catppuccin Macchiato"
-
--- font
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 12
-
 -- Open full screen
 wezterm.on("gui-startup", function()
 	local mux = wezterm.mux
 	local _, _, window = mux.spawn_window({})
 	window:gui_window():maximize()
 end)
+
+-- Hide tab bar
+config.enable_tab_bar = false
+
+-- Only allow resizing decorations
+config.window_decorations = "RESIZE"
+
+-- Full opacity
+config.window_background_opacity = 1
+
+-- Reload config when file changes
+config.automatically_reload_config = true
+
+-- Set theme
+config.color_scheme = "Catppuccin Macchiato"
+
+-- Font
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 12
 
 return config
