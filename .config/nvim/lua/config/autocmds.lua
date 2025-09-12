3b673eda9f903b76c939ac8b829d2c6836abd4d4
Gokay Gok <gokgokay35@gmail.com>
2025-09-12 13:07:17 +0300

    feat(autocmds): add filetype handling and highlights

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,18 +1,21 @@
 -- This file is automatically loaded by config.lua
+local autocmd = vim.api.nvim_create_autocmd
+local sethl = vim.api.nvim_set_hl
+
 local function augroup(name)
 	return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
 end
 
--- Highlight on yank (copy)
-vim.api.nvim_create_autocmd("TextYankPost", {
+-- Highlight on yank
+autocmd("TextYankPost", {
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 	callback = function()
-		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+		vim.highlight.on_yank({ higroup = "YankHighlight", timeout = 200 })
 	end,
 })
 
 -- Resize splits if window got resized
-vim.api.nvim_create_autocmd({ "VimResized" }, {
+autocmd({ "VimResized" }, {
 	group = augroup("resize_splits"),
 	callback = function()
 		local current_tab = vim.fn.tabpagenr()
@@ -22,7 +25,7 @@ vim.api.nvim_create_autocmd({ "VimResized" }, {
 })
 
 -- Go to last loc when opening a buffer
-vim.api.nvim_create_autocmd("BufReadPost", {
+autocmd("BufReadPost", {
 	group = augroup("last_loc"),
 	callback = function(event)
 		local exclude = { "gitcommit" }
@@ -39,8 +42,8 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 	end,
 })
 
--- Automatically enables line wrapping and spell checking
-vim.api.nvim_create_autocmd({ "FileType" }, {
+-- Enable line wrapping and spell checking
+autocmd({ "FileType" }, {
 	pattern = { "gitcommit", "markdown", "NeogitCommitMessage", "txt" },
 	callback = function()
 		vim.opt_local.wrap = true
@@ -56,19 +59,108 @@ vim.diagnostic.config({
 	update_in_insert = false,
 })
 
--- Use "q" or "<Esc>" to close special buffers
-vim.api.nvim_create_autocmd("FileType", {
+-- Close some filetypes with q or <esc>
+autocmd("FileType", {
 	pattern = {
 		"checkhealth",
 		"lspinfo",
 		"lazy",
 		"help",
+		"neotest-output",
 		"neotest-output-panel",
 		"neotest-summary",
+		"notify",
+		"neogitstatus",
 	},
-	command = [[
-          nnoremap <buffer><silent> q :close<CR>
-          nnoremap <buffer><silent> <esc> :close<CR>
-          set nobuflisted
-      ]],
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.schedule(function()
+			local close_fn = function()
+				vim.cmd("close")
+				pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+			end
+			vim.keymap.set("n", "q", close_fn, {
+				buffer = event.buf,
+				silent = true,
+				desc = "Quit buffer",
+			})
+			vim.keymap.set("n", "<esc>", close_fn, {
+				buffer = event.buf,
+				silent = true,
+				desc = "Quit buffer",
+			})
+		end)
+	end,
+})
+
+-- Move help buffer to the left side automatically
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "help",
+	callback = function()
+		vim.cmd("wincmd H")
+	end,
+})
+
+-- Disable indentscope, number and cursorline for the specific filetypes
+autocmd("FileType", {
+	pattern = {
+		"neo-tree",
+		"NvimTree",
+		"lazy",
+		"help",
+		"checkhealth",
+		"git",
+		"gitcommit",
+		"lazyterm",
+		"lspinfo",
+		"mason",
+		"notify",
+		"query",
+		"Trouble",
+		"toggleterm",
+		"dashboard",
+	},
+	callback = function()
+		vim.opt_local.number = false
+		vim.opt_local.cursorline = false
+		vim.b.miniindentscope_disable = true
+	end,
+})
+
+-- Custom highlights
+autocmd({ "BufWinEnter", "ColorScheme" }, {
+	group = augroup("colors"),
+	pattern = { "*" },
+	callback = function()
+		local colors = {
+			base = "#1e2030",
+			base2 = "#23273c",
+			line = "#494d64",
+			text = "#cad3f5",
+			surface0 = "#363a4f",
+			peach = "#f5a97f",
+			blue = "#8aadf4",
+			sunny = "#f9e2af",
+			black = "#000000",
+		}
+
+		-- telescope
+		sethl(0, "TelescopeResultsBorder", { fg = colors.line, bg = colors.base })
+		sethl(0, "TelescopePreviewBorder", { fg = colors.line, bg = colors.base })
+		sethl(0, "TelescopeBorder", { fg = colors.line, bg = colors.base })
+		sethl(0, "TelescopePromptBorder", { fg = colors.line, bg = colors.base })
+		sethl(0, "TelescopeResultsNormal", { fg = colors.text, bg = colors.base })
+		sethl(0, "TelescopePromptNormal", { fg = colors.text, bg = colors.base })
+		sethl(0, "TelescopePreviewNormal", { fg = colors.text, bg = colors.base })
+		sethl(0, "TelescopePreviewTitle", { fg = colors.text, bg = colors.base })
+		sethl(0, "TelescopeResultsTitle", { fg = colors.text, bg = colors.base })
+		sethl(0, "TelescopeSelection", { fg = colors.text, bg = colors.surface0, bold = true })
+		sethl(0, "TelescopeMatching", { fg = colors.peach })
+
+		-- yank
+		sethl(0, "YankHighlight", { fg = colors.black, bg = colors.sunny })
+
+		-- bufferline
+		sethl(0, "BufferLineFill", { bg = colors.base })
+	end,
 })
