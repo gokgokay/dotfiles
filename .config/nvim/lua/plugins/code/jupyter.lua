fb01d12159f9d7be924ede3ddabcab45f7830661
Gokay Gok <gokgokay35@gmail.com>
2025-09-24 17:51:59 +0300

    refactor(jupyter): rename iron to jupyter and simplify opts

--- .config/nvim/lua/plugins/code/jupyter.lua
+++ .config/nvim/lua/plugins/code/jupyter.lua
@@ -1,11 +1,10 @@
 return {
+	-- Repl
 	"hkupty/iron.nvim",
 	event = "VeryLazy",
 	opts = {
 		config = {
-			scratch_repl = true,
 			repl_open_cmd = "vertical rightbelow 90 split",
-			ignore_blank_lines = true,
 		},
 		keymaps = {
 			toggle_repl = "<space>rr",
@@ -13,6 +12,7 @@ return {
 			send_line = "<space>rl",
 			send_code_block_and_move = "<space>rb",
 			send_paragraph = "<space>rp",
+			send_until_cursor = "<space>rt",
 			restart_repl = "<space>rR",
 			interrupt = "<space>ri",
 			clear = "<space>rc",
