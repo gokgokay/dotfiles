5bc375f3a1b18dda7a547b5aaf624d289a92ac0c
Gokay Gok <gokgokay35@gmail.com>
2025-10-03 12:16:51 +0300

    Some minor changes

--- .config/nvim/lua/plugins/code/trouble.lua
+++ .config/nvim/lua/plugins/code/trouble.lua
@@ -1,15 +1,13 @@
 return {
 	-- Better diagnostics list and others
-	{
-		"folke/trouble.nvim",
-		cmd = "Trouble",
-		keys = {
-			{
-				"<leader>xx",
-				"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
-				desc = "Buffer diagnostics",
-			},
-			{ "<leader>xX", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics" },
+	"folke/trouble.nvim",
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
+			desc = "Buffer diagnostics",
 		},
+		{ "<leader>xX", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics" },
 	},
 }
