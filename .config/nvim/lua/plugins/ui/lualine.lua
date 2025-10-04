dacc31a964205445d7df028432c49061c28ebafa
Gokay Gok <gokgokay35@gmail.com>
2025-10-04 17:20:29 +0300

    refactor: simplify config and add extensions

--- .config/nvim/lua/plugins/ui/lualine.lua
+++ .config/nvim/lua/plugins/ui/lualine.lua
@@ -1,33 +1,39 @@
 return {
 	-- Statusline customization
-	{
-		"nvim-lualine/lualine.nvim",
-		lazy = false,
-		event = "VimEnter",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-			"arkav/lualine-lsp-progress",
+	"nvim-lualine/lualine.nvim",
+	event = "VimEnter",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"arkav/lualine-lsp-progress",
+	},
+	opts = {
+		options = {
+			theme = "auto",
+			section_separators = { left = "", right = "" },
+			component_separators = { left = "", right = "" },
+			globalstatus = true,
+			disabled_filetypes = {
+				statusline = {
+					"dashboard",
+					"lazy",
+					"mason",
+				},
+			},
 		},
-		opts = {
-			options = {
-				theme = "auto",
-				section_separators = { left = "", right = "" },
-				component_separators = { left = "", right = "" },
-				disabled_filetypes = { statusline = { "dashboard", "lazy" } },
-				globalstatus = true,
-			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch", "diff" },
-				lualine_c = { { "filename", path = 1 } },
-				lualine_x = { "diagnostics", "filetype" },
-				lualine_y = { "encoding", "fileformat" },
-				lualine_z = { "location", "progress" },
-			},
-			inactive_sections = {
-				lualine_c = { "filename" },
-				lualine_x = { "location" },
-			},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff" },
+			lualine_c = { { "filename" } },
+			lualine_x = { "diagnostics", "filetype" },
+			lualine_y = { "encoding", "fileformat" },
+			lualine_z = { "location", "progress" },
+		},
+		extensions = {
+			"nvim-tree",
+			"nvim-dap-ui",
+			"mason",
+			"lazy",
+			"trouble",
 		},
 	},
 }
