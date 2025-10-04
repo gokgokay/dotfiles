39af68c9555cb23c5d91c13ff6b7b5750bb46da4
Gokay Gok <gokgokay35@gmail.com>
2025-10-04 17:41:21 +0300

    refactor: simplify theme config

--- .config/nvim/lua/plugins/ui/colorscheme.lua
+++ .config/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,34 +1,32 @@
 return {
 	-- Theme setup
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			integrations = {
-				cmp = true,
-				gitsigns = true,
-				nvimtree = true,
-				notify = true,
-				treesitter = true,
-				treesitter_context = true,
-				neotest = true,
-				which_key = true,
-				markdown = true,
-				mason = true,
-				fzf = true,
-				flash = true,
-				indent_blankline = { enabled = true },
-			},
-			background = {
-				light = "latte",
-				dark = "macchiato",
-			},
+	"catppuccin/nvim",
+	name = "catppuccin",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		integrations = {
+			cmp = true,
+			gitsigns = true,
+			nvimtree = true,
+			notify = true,
+			treesitter = true,
+			treesitter_context = true,
+			neotest = true,
+			which_key = true,
+			markdown = true,
+			mason = true,
+			fzf = true,
+			flash = true,
+			indent_blankline = { enabled = true },
 		},
-		config = function(_, opts)
-			require("catppuccin").setup(opts)
-			vim.cmd.colorscheme("catppuccin-macchiato")
-		end,
+		background = {
+			light = "latte",
+			dark = "macchiato",
+		},
 	},
+	config = function(_, opts)
+		require("catppuccin").setup(opts)
+		vim.cmd.colorscheme("catppuccin-macchiato")
+	end,
 }
