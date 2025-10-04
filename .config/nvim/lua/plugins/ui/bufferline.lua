eb2c9325adddffbdd5f28dc338cccfc8331a7837
Gokay Gok <gokgokay35@gmail.com>
2025-10-04 17:39:53 +0300

    refactor: simlify config

--- .config/nvim/lua/plugins/ui/bufferline.lua
+++ .config/nvim/lua/plugins/ui/bufferline.lua
@@ -1,46 +1,38 @@
 return {
 	-- Buffer management
-	{
-		"akinsho/bufferline.nvim",
-		dependencies = { "echasnovski/mini.bufremove" },
-		event = "VeryLazy",
-		init = function()
-			local bufline = require("catppuccin.groups.integrations.bufferline")
-			function bufline.get()
-				return bufline.get_theme()
-			end
-		end,
-		opts = {
-			options = {
-				diagnostics = "nvim_lsp",
-				always_show_bufferline = false,
-				diagnostics_indicator = function(count, level)
-					local icon = level:match("error") and " " or " "
-					return " " .. icon .. count
-				end,
-			},
+	"akinsho/bufferline.nvim",
+	dependencies = { "echasnovski/mini.bufremove" },
+	event = "VeryLazy",
+	opts = {
+		options = {
+			diagnostics = "nvim_lsp",
+			always_show_bufferline = false,
+			diagnostics_indicator = function(count, level)
+				local icon = level:match("error") and " " or " "
+				return " " .. icon .. count
+			end,
 		},
-		keys = {
-			{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
-			{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-			{ "<leader>bb", "<cmd>e #<cr>", desc = "Switch buffer" },
-			{
-				"<leader>bd",
-				function()
-					require("mini.bufremove").delete(0, false)
-				end,
-				desc = "Delete buffer",
-			},
-			{
-				"<leader>bD",
-				function()
-					require("mini.bufremove").delete(0, true)
-				end,
-				desc = "Force delete buffer",
-			},
-			{ "<leader>bo", "<cmd>BufferLineCloseOthers<cr>", desc = "Close other buffers" },
-			{ "<leader>bp", "<cmd>BufferLineTogglePin<cr>", desc = "Toggle pin buffer" },
-			{ "<leader>bP", "<cmd>BufferLineGroupClose ungrouped<cr>", desc = "Close non-pinned buffers" },
+	},
+	keys = {
+		{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+		{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+		{ "<leader>bb", "<cmd>e #<cr>", desc = "Switch buffer" },
+		{
+			"<leader>bd",
+			function()
+				require("mini.bufremove").delete(0, false)
+			end,
+			desc = "Delete buffer",
 		},
+		{
+			"<leader>bD",
+			function()
+				require("mini.bufremove").delete(0, true)
+			end,
+			desc = "Force delete buffer",
+		},
+		{ "<leader>bo", "<cmd>BufferLineCloseOthers<cr>", desc = "Close other buffers" },
+		{ "<leader>bp", "<cmd>BufferLineTogglePin<cr>", desc = "Toggle pin buffer" },
+		{ "<leader>bP", "<cmd>BufferLineGroupClose ungrouped<cr>", desc = "Close non-pinned buffers" },
 	},
 }
