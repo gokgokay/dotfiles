e241e8b601b64eda071b8079ad7d4ed7cf260ad7
Gokay Gok <gokgokay35@gmail.com>
2025-10-05 20:12:11 +0300

    refactor: update keymaps

--- .config/nvim/lua/plugins/editor/auto-session.lua
+++ .config/nvim/lua/plugins/editor/auto-session.lua
@@ -5,14 +5,15 @@ return {
 		lazy = false,
 		opts = {
 			bypass_save_filetypes = { "dashboard" },
-			pre_save_cmds = { "NvimTreeClose" },
+			pre_save_cmds = { "tabdo NvimTreeClose" },
 			close_filetypes_on_save = { "checkhealth", "iron" },
 		},
 		keys = {
-			{ "<leader>qd", "<cmd>SessionDelete<cr>", desc = "Delete session" },
-			{ "<leader>qf", "<cmd>SessionSearch<cr>", desc = "Find session" },
-			{ "<leader>qr", "<cmd>SessionRestore<cr>", desc = "Restore session" },
-			{ "<leader>qs", "<cmd>SessionSave<cr>", desc = "Save session" },
+			{ "<leader>qd", "<cmd>AutoSession delete<cr>", desc = "Delete session" },
+			{ "<leader>qf", "<cmd>AutoSession search<cr>", desc = "Find session" },
+			{ "<leader>qp", "<cmd>AutoSession deletePicker<cr>", desc = "Pick sesssion to delete" },
+			{ "<leader>qr", "<cmd>AutoSession restore<cr>", desc = "Restore session" },
+			{ "<leader>qs", "<cmd>AutoSession save<cr>", desc = "Save session" },
 		},
 	},
 }
