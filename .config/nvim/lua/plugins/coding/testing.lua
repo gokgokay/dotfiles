891347f0ae6352649797381127b6849d0ec51917
Gokay Gok <48049961+gokgokay@users.noreply.github.com>
2026-07-18 18:05:23 +0300

    feat(testing): run tests in tmux pane

--- .config/nvim/lua/plugins/coding/testing.lua
+++ .config/nvim/lua/plugins/coding/testing.lua
@@ -1,14 +1,20 @@
 return {
-	-- Easy test runner
+	-- Test runner
 	{
 		"vim-test/vim-test",
 		dependencies = { "preservim/vimux" },
+		config = function()
+			-- run tests in tmux pane
+			vim.g["test#strategy"] = "vimux"
+			-- use pytest for python
+			vim.g["test#python#runner"] = "pytest"
+		end,
 		keys = {
-			{ "<leader>tt", "<cmd>:TestNearest<cr>", desc = "Test nearest" },
-			{ "<leader>tT", "<cmd>:TestFile<cr>", desc = "Test files" },
-			{ "<leader>ta", "<cmd>:TestSuite<cr>", desc = "Test suite" },
-			{ "<leader>tl", "<cmd>:TestLast<cr>", desc = "Test last" },
-			{ "<leader>tg", "<cmd>:TestVisit<cr>", desc = "Test visit" },
+			{ "<leader>tt", "<cmd>TestNearest<cr>", desc = "Test nearest" },
+			{ "<leader>tT", "<cmd>TestFile<cr>", desc = "Test files" },
+			{ "<leader>ta", "<cmd>TestSuite<cr>", desc = "Test suite" },
+			{ "<leader>tl", "<cmd>TestLast<cr>", desc = "Test last" },
+			{ "<leader>tg", "<cmd>TestVisit<cr>", desc = "Test visit" },
 		},
 	},
 }
