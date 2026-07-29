6e9f06a91d62d790f82b27cbf2eaa86e81c12f13
Gokay Gok <48049961+gokgokay@users.noreply.github.com>
2026-07-29 22:55:10 +0300

    refactor(colorscheme): set flavour in opts

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,11 +6,8 @@ return {
 		lazy = false,
 		priority = 1000,
 		opts = {
+			flavour = "macchiato",
 			auto_integrations = true,
 		},
-		config = function(_, opts)
-			require("catppuccin").setup(opts)
-			vim.cmd.colorscheme("catppuccin-macchiato")
-		end,
 	},
 }
