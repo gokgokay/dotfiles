dcc5748c7b6116e64198d80ca08f8b6a6e571f55
Gokay Gok <48049961+gokgokay@users.noreply.github.com>
2026-07-30 03:16:52 +0300

    feat(todo-comments): add jump keymaps

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,15 +1,25 @@
 return {
-	-- Highlight todo, notes, etc in comments
+	-- Highlight todos
 	{
 		"folke/todo-comments.nvim",
-		lazy = true,
 		dependencies = { "nvim-lua/plenary.nvim" },
 		keys = {
 			{ "<leader>st", "<cmd>TodoTelescope keywords=TODO,FIX,HACK<cr>", desc = "Todo/Fix/Hack" },
-			{ "<leader>sq", "<cmd>TodoQuickFix<cr>", desc = "Quickfix todos" },
+			{ "<leader>sq", "<cmd>TodoQuickFix<cr>", desc = "Quickfix todo" },
+			{
+				"]t",
+				function()
+					require("todo-comments").jump_next()
+				end,
+				desc = "Next todo",
+			},
+			{
+				"[t",
+				function()
+					require("todo-comments").jump_prev()
+				end,
+				desc = "Prev todo",
+			},
 		},
-		config = function()
-			require("todo-comments").setup()
-		end,
 	},
 }
